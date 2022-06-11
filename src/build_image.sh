775629b9630787dd4a59bbdc4267c65225bd573c
Arnaud Blois <hi@arnaudblois.com>
2022-06-11 23:06:33 +0100

    passing as ARG OPENSSL_VERSION

--- src/build_image.sh
+++ src/build_image.sh
@@ -11,4 +11,5 @@ docker build . \
   --build-arg PY_VERSION=${PYTHON_VERSION} \
   --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
   --build-arg CREATION_COMMIT_SHA="${GITHUB_SHA}" \
-  --build-arg QUICK_BUILD="${QUICK_BUILD}"
+  --build-arg QUICK_BUILD="${QUICK_BUILD}" \
+  --build-arg OPENSSL_VERSION="${OPENSSL_VERSION}"
