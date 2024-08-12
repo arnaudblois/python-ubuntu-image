e15f18f30dc537d1f798dd6c557221dd44d7cbb1
Arnaud Blois <hi@arnaudblois.com>
2024-08-12 23:51:25 +0100

    August update: Python 3.12.5 and 3.13.0rc1, openssl 3.3.1 + linting of .py (#27)

--- src/test_image.py
+++ src/test_image.py
@@ -1,12 +1,11 @@
-import os
+"""Test the image by making sure openssl is correctly linked."""
+
 import ssl
 
 import subprocess
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     openssl = subprocess.run(
-        ["/usr/local/ssl/bin/openssl", "version"],
-        capture_output=True,
-        text=True
+        ["/usr/local/ssl/bin/openssl", "version"], capture_output=True, text=True
     ).stdout
     assert ssl.OPENSSL_VERSION in openssl
